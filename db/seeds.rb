b69f06be72a9535829937de608054d256b25c0d3
Julius Rückin <julius.rueckin@rats-os.de>
2015-10-24 12:39:55 +0200

    added default database content via seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+subjects = ["Mathematik 1", "Modellierung 1", "Grundlagen digitaler Systeme"]
+profs = ["Prof. Meinel", "Prof. Giese", "Dr. Wollowski"]
+
+subjects.each_with_index do |subject,i|
+	sub_entry = Subject.create :name => subject
+	Prof.create :name => profs[i], :subject_id => sub_entry.id
+end
+
+events = ["Vorlesung", "Übung"]
+events.each do |event|
+	Event.create :name => event
+end
+
+handing_ins = ["Klausur", "Testat", "Präsentation", "Hausarbeit"]
+handing_ins.each do |handing_in|
+	HandingIn.create :name => handing_in
+end
