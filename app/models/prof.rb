0310c8caf978a196ceeebb76ddfd05132c0c4dd8
Julius Rückin <julius.rueckin@rats-os.de>
2015-10-31 16:05:30 +0100

    added er-diagram

--- app/models/prof.rb
+++ app/models/prof.rb
@@ -3,5 +3,6 @@ class Prof < ActiveRecord::Base
 	belongs_to :subject
 
 	validates :name, :presence => true, :uniqueness => true
+	validates :subject_id, :presence => true, :uniqueness => true
 
 end
