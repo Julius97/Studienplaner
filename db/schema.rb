fe26191ac10c5a21faa387b337778c3e4d6fcdf0
Julius97 <julius.rueckin@rats-os.de>
2015-11-03 23:36:12 +0100

    Added code snippet used during presentation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151023204237) do
+ActiveRecord::Schema.define(version: 20151023150356) do
 
   create_table "events", force: true do |t|
     t.string   "name"
